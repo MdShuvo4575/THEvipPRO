a2f4266aaf3f972b1a50fcc539d17b4e382a0ec9
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-08-31 20:51:42 +0800

    Update __main__.py

--- THEElianaPROBot/__main__.py
+++ THEElianaPROBot/__main__.py
@@ -73,17 +73,9 @@ def get_readable_time(seconds: int) -> str:
     return ping_time
 
 
-PM_START_TEXT = """
+PM_START_TEXT = f"""
 Hello, I'M THE Eliana PRO Bot
-I'm Here to Help You Manage Your Groups! 
-
-🔛 Helpful commands :
-
-1⃣ /start: Starts me! You've probably already used this.
-2⃣ /help: Sends this message; I'll tell you more about myself!
-3⃣ /donate: Gives you info on how to support me and my creator.
-⏺ If you have any question or bugs @TEP_Discussion.
-
+I'm Here to Help You Manage Your Groups!
 ☑️ Maintained by @THEElianaPRO ❤️
 """
 
