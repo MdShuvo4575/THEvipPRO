21552eed3b0041683d659ce022b0d44eb9fcc432
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-09-01 08:44:40 +0800

    Update purge.py

--- THEElianaPROBot/modules/purge.py
+++ THEElianaPROBot/modules/purge.py
@@ -77,6 +77,12 @@ DEL_HANDLER = delete_messages, events.NewMessage(pattern="^[!/]del$")
 telethn.add_event_handler(*PURGE_HANDLER)
 telethn.add_event_handler(*DEL_HANDLER)
 
-__mod_name__ = "DETELE"
+__mod_name__ = "Purges"
 __command_list__ = ["del", "purge"]
 __handlers__ = [PURGE_HANDLER, DEL_HANDLER]
+
+__help__ = """
+ ❍ /del*:* deletes the message you replied to
+ ❍ /purge*:* deletes all messages between this and the replied to message.
+ ❍ /purge <integer X>*:* deletes the replied message, and X messages following it if replied to a message.
+"""
