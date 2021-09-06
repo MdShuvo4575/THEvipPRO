bec7b5d215463e0c2f12659d2e8a120823aa3226
THEvipPRO <90199464+THEvipPRO@users.noreply.github.com>
2021-09-07 04:34:28 +0800

    Update welcome.py

--- THEElianaPROBot/modules/welcome.py
+++ THEElianaPROBot/modules/welcome.py
@@ -240,7 +240,7 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome yourself
             elif new_mem.id == bot.id:
                 update.effective_message.reply_text(
-                    "Thanks for adding me! Join @AwesomeSupport for support.",
+                    "Thanks for adding me! Join @TVB_Discussion for support.",
                     reply_to_message_id=reply,
                 )
                 continue
